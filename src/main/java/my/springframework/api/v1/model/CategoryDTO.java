82ce00cabf556bb76e79fba30167a44988287f0c
SmallAd <vadim.piontkovskiy@gmail.com>
2020-07-18 14:29:02 +0300

    add initial model

---
+++ src/main/java/my/springframework/api/v1/model/CategoryDTO.java
@@ -0,0 +1,6 @@
+package my.springframework.api.v1.model;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/my/springframework/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package my.springframework.repositories;
+
+import my.springframework.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
