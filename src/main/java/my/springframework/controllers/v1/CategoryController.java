4bfbe14e1bfd8bed2ec57d2327b380beabf5e822
SmallAd <vadim.piontkovskiy@gmail.com>
2020-07-21 23:09:08 +0300

    RestController added

--- src/main/java/my/springframework/controllers/v1/CategoryController.java
+++ src/main/java/my/springframework/controllers/v1/CategoryController.java
@@ -5,27 +5,25 @@ import my.springframework.api.v1.model.CategoryDTO;
 import my.springframework.api.v1.model.CategoryListDTO;
 import my.springframework.services.CategoryService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/api/v1/categories")
+@RestController
+@RequestMapping(CategoryController.BASE_URL)
 @RequiredArgsConstructor
 public class CategoryController {
 
+    public static final String BASE_URL = "/api/v1/categories";
     private final CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<CategoryListDTO> getAllCategories() {
-        return new ResponseEntity<>(
-                new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryListDTO getAllCategories() {
+        return new CategoryListDTO(categoryService.getAllCategories());
     }
 
     @GetMapping("/{name}")
-    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
-        return new ResponseEntity<>(categoryService.getByName(name), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName(@PathVariable String name) {
+        return categoryService.getByName(name);
     }
 }
--- src/test/java/my/springframework/controllers/v1/AbstractRestControllerTest.java
+++ src/test/java/my/springframework/controllers/v1/AbstractRestControllerTest.java
@@ -1,6 +1,5 @@
 package my.springframework.controllers.v1;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public abstract class AbstractRestControllerTest {
